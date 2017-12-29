bfb43540d2f0a351419bb930dd44e874812d99ea
Kyrylo Fedorov <kyryloFedorov@gmail.com>
2017-12-29 15:19:34 +0200

    Very first approach, heavily inspired by eslint-plugin-react/js-curly-bracer-presence
    - includes two rule;
    - some tests for them;

---
+++ tests/lib/rules/translate-props-literals.js
@@ -0,0 +1,36 @@
+const rule = require('../../../lib/rules/translate-props-literals');
+const RuleTester = require('eslint').RuleTester;
+const parserOptions = {
+    sourceType: 'module',
+    ecmaFeatures: {
+        jsx: true
+    }
+};
+
+// const missingCurlyMessage = 'Need to wrap this literal in a JSX expression.';
+const missingTranslationMessage = 'Need to translate this literal in JSX prop.';
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({parserOptions});
+
+ruleTester.run('translate-props-literals', rule, {
+    valid: [
+        {
+            code: '<App><Component /></App>'
+        },
+        {
+            code: '<input placeholder={formatMessage(message)} />'
+        }
+    ],
+    invalid: [
+        {
+            code: '<input name="inputElement" placeholder="untranslated message" />',
+            output: '<input name="inputElement" placeholder={formatMessage(defineMessages({id: "placeholder", defaultMessage: "untranslated message", description: "placeholder"}))} />',
+            options: [{props: ['placeholder']}],
+            errors: [{message: missingTranslationMessage}]
+        }
+    ]
+});
